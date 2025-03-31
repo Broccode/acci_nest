041a2858c2ab36c2a6d55a6409aa10d812a9cf49
Walloschke, Michael <michael.walloschke@googlemail.com>
2025-04-01 00:49:05 +0200

    chore: clean up dist directory and update .gitignore
    - Removed all generated files from the dist directory to ensure a clean build environment.
    - Added 'dist' to .gitignore to prevent future inclusion of generated files in version control.

--- src/backend/dist/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { EntityManager } from '@mikro-orm/core';
-export declare class AppService {
-    private readonly em;
-    constructor(em: EntityManager);
-    getHello(): string;
-    checkDbConnection(): Promise<boolean>;
-}
